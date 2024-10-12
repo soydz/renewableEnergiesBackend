7c3362dfbcc565ea04aa56f382cb80700e864548
soydz <dev@soydz.com>
2024-10-12 11:00:14 -0500

    Se crean las operaciones crud para ConsumptionEnergy

--- src/main/java/com/ProyectRenewableEnergiesBackend/controller/ConsumptionEnergyController.java
+++ src/main/java/com/ProyectRenewableEnergiesBackend/controller/ConsumptionEnergyController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/consumption")
@@ -27,5 +28,27 @@ public class ConsumptionEnergyController {
     public ResponseEntity<List<ConsumptionEnergy>> getAll() {
         List<ConsumptionEnergy> consumptionEnergyList = consumptionEnergyService.getAll();
         return new ResponseEntity<>(consumptionEnergyList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ConsumptionEnergy> getById(@PathVariable("id") int id) {
+        Optional<ConsumptionEnergy> consumptionEnergy = consumptionEnergyService.getById(id);
+        return consumptionEnergy.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ConsumptionEnergy> updateById(@PathVariable("id") int id, @RequestBody ConsumptionEnergy consumption) {
+        ConsumptionEnergy newConsumtion = new ConsumptionEnergy();
+        if(consumption.getValue() != null) {
+            newConsumtion.setValue(consumption.getValue());
+        }
+        ConsumptionEnergy updateConsumption = consumptionEnergyService.updateById(id, newConsumtion);
+        return new ResponseEntity<>(updateConsumption, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ConsumptionEnergy> deleteById(@PathVariable("id") int id) {
+        consumptionEnergyService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/com/ProyectRenewableEnergiesBackend/service/ConsumptionEnergyService.java
+++ src/main/java/com/ProyectRenewableEnergiesBackend/service/ConsumptionEnergyService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ConsumptionEnergyService {
@@ -19,5 +20,24 @@ public class ConsumptionEnergyService {
 
     public List<ConsumptionEnergy> getAll() {
         return consumptionEnergyRepository.findAll();
+    }
+
+    public Optional<ConsumptionEnergy> getById(int id){
+        return consumptionEnergyRepository.findById(id);
+    }
+
+    public ConsumptionEnergy updateById(int id, ConsumptionEnergy consumption) {
+        return consumptionEnergyRepository.findById(id)
+                .map(oldConsumption -> {
+                    if(consumption.getValue() != null) {
+                        oldConsumption.setValue(consumption.getValue());
+                    }
+                    return consumptionEnergyRepository.save(oldConsumption);
+                })
+                .orElseThrow(() -> new RuntimeException("Location with id " + id + " not found"));
+    }
+
+    public void deleteById(int id) {
+        consumptionEnergyRepository.deleteById(id);
     }
 }
