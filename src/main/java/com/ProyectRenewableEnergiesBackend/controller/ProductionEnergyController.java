a25b908c682d8c701761d41f3ecc22263aafeeb9
soydz <dev@soydz.com>
2024-10-12 10:51:50 -0500

    Se crean las operaciones crud para ProductionEnergy

--- src/main/java/com/ProyectRenewableEnergiesBackend/controller/ProductionEnergyController.java
+++ src/main/java/com/ProyectRenewableEnergiesBackend/controller/ProductionEnergyController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/production")
@@ -26,6 +27,28 @@ public class ProductionEnergyController {
     public ResponseEntity<List<ProductionEnergy>> getAll() {
         List<ProductionEnergy> productionEnergyList = productionEnergyService.getAll();
         return new ResponseEntity<>(productionEnergyList, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductionEnergy> getById(@PathVariable("id") int id) {
+        Optional<ProductionEnergy> productionEnergy = productionEnergyService.getById(id);
+        return productionEnergy.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductionEnergy> updateById(@PathVariable("id") int id, @RequestBody ProductionEnergy production) {
+        ProductionEnergy newProduction = new ProductionEnergy();
+        if(production.getValue() != null) {
+            newProduction.setValue(production.getValue());
+        }
+        ProductionEnergy updateProduction = productionEnergyService.updateById(id, newProduction);
+        return new ResponseEntity<>(updateProduction, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductionEnergy> deleteById(@PathVariable("id") int id) {
+        productionEnergyService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
--- src/main/java/com/ProyectRenewableEnergiesBackend/service/ProductionEnergyService.java
+++ src/main/java/com/ProyectRenewableEnergiesBackend/service/ProductionEnergyService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductionEnergyService {
@@ -20,5 +21,24 @@ public class ProductionEnergyService {
 
     public List<ProductionEnergy> getAll() {
         return productionEnergyRepository.findAll();
+    }
+
+    public Optional<ProductionEnergy> getById(int id){
+        return productionEnergyRepository.findById(id);
+    }
+
+    public ProductionEnergy updateById(int id, ProductionEnergy production) {
+        return productionEnergyRepository.findById(id)
+                .map(oldProduction -> {
+                    if(production.getValue() != null) {
+                        oldProduction.setValue(production.getValue());
+                    }
+                    return productionEnergyRepository.save(oldProduction);
+                })
+                .orElseThrow(() -> new RuntimeException("Location with id " + id + " not found"));
+    }
+
+    public void deleteById(int id) {
+        productionEnergyRepository.deleteById(id);
     }
 }
